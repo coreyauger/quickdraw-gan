369cb70560bd5c4cf5198338fef4cb6480b89199
Corey Auger <coreyauger@gmail.com>
2018-02-28 19:04:30 -0800

    matplotlib

--- quickdraw.py
+++ quickdraw.py
@@ -8,16 +8,18 @@ from keras.layers import Input, Dense, Conv2D, BatchNormalization, Dropout, Flat
 from keras.layers import Activation, Reshape, Conv2DTranspose, UpSampling2D
 from keras.optimizers import RMSprop
 
-import pandas as pd
+import pandas as pd 
+import matplotlib
 from matplotlib import pyplot as plt
+matplotlib.interactive(True)
 
-input_images = "../data/car.npy"
+input_images = "./data/car.npy"
 data = np.load(input_images)
 data = data/255
 img_w, img_h = 28, 28
 data = np.reshape(data, [data.shape[0], img_w, img_h, 1])
 print(data.shape)
 
-plt.imgshow(data[0,:,:,0], cmap="Greys")
+plt.imshow(data[0,:,:,0], cmap="Greys")
 
 input("Press Enter to exit")
